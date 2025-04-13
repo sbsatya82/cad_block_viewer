9a946b479ae2727bea19dbc22d71f1a7f61b2845
sbsatya82 <satyabratamahanta82@gmail.com>
2025-04-13 21:53:22 +0530

    bug fix

--- frontend/src/api/axiosApi.js
+++ frontend/src/api/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosAPI = axios.create({
-  baseURL: 'http://localhost:5000/api', // Change port if needed
+  baseURL: import.meta.env.MODE === 'production'? '/api': 'http://localhost:5000/api', // Change port if needed
 });
 
 export default axiosAPI;
