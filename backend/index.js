b3835856b2499e8c9e3c3733508e473305e681e3
sbsatya82 <satyabratamahanta82@gmail.com>
2025-04-13 21:46:03 +0530

    bug fix

--- backend/index.js
+++ backend/index.js
@@ -8,31 +8,29 @@ import uploadRoutes from './routes/upload.js';
 import blockRoutes from './routes/blocks.js';
 
 
-
-
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-const __dirname = path.resolve();
 
 const port = process.env.PORT || 3000;
+
+const __dirname = path.resolve(); 
 
 app.use('/api/upload', uploadRoutes);
 app.use('/api/blocks', blockRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
-  app.get("*",(req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+  app.get("/", (req,res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
   })
 }
+
+
 
 dbConnect.sync().then(() => {
   app.listen(port, () => {
